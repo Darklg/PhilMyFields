6c8bd9cf1440031c9b08d4faa0334b807feb4e7c
Kevin <kevin@colorz.fr>
2011-12-12 09:50:25 +0100

    fix adress

--- phil.js
+++ phil.js
@@ -132,7 +132,7 @@ function phil_my_fields(){
 			el.value = phil_random('pseudo');
 		} else if(phil_test(el.name,['cc_number'],'or') || el.name == 'cp'){
 			el.value = '4111111111111111';
-		} else if(phil_test(el.name,['telephone','tel','mobile','fax'],'or') || el.name == 'tel'|| el.name == 'fax'){
+		} else if(phil_test(el.name,['telephone','tel_','mobile','fax'],'or') || el.name == 'tel'|| el.name == 'fax'){
 			el.value = '014'+phil_random_string('digit',7);
 		} else if(phil_test(el.name,['city','ville'],'or')){
 			el.value = 'Paris';
@@ -146,10 +146,10 @@ function phil_my_fields(){
 			el.value = phil_random('nom');
 		} else if(phil_test(el.name,['societe','company'],'or')){
 			el.value = phil_random('nom')+' Inc.';
-		} else if(phil_test(el.name,['adresse','address','street','adr'],'or')){
-			el.value = phil_random('adresse');
 		} else if(phil_test(el.name,['code','postal','zipcode','zip','cp'],'or') || el.name == 'cp'){
 			el.value = '75001';
+		} else if(phil_test(el.name,['adresse','address','street','adr'],'or')){
+			el.value = phil_random('adresse');
 		} else if(el.type == 'password'){
 			el.value = 'phil1phil';
 		}
